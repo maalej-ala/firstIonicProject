9dfc8353412b755b068fbb02035668490d38b240
Louay kahouli <louay.kahouli@ensit.u-tunis.tn>
2025-04-12 04:28:21 +0200

    sign up2

--- src/app/models/client.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Client {
-    id: number;
-    name: string;
-    phone: string;
-    image?: string;  // Optional property
-    // Add any other properties your clients have
-    // email?: string;
-    // address?: string;
-  }
